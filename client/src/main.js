a26b41c6563af7ab456be66c02a27c399339c880
tardigrada777 <ntwrite@gmail.com>
2019-07-03 23:12:20 +0300

    ApolloQuery component

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,22 @@ import router from "./router";
 import store from "./store";
 import "babel-polyfill";
 
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+
+Vue.use(VueApollo);
+
 Vue.config.productionTip = false;
 
+// Setup ApolloClient
+const defaultClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const apolloProvider = new VueApollo({ defaultClient });
+
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   store,
   render: h => h(App)
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,42 @@
 <template>
   <v-container>
     <h1>Home</h1>
+    <ApolloQuery :query="getPostsQuery">
+      <template slot-scope="{ result: { error, data, networkStatus}, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else-if="error">Error! {{ error.message }}</div>
+        <ul v-else>
+          <li
+            v-for="post in data.getPosts"
+            :key="post.title"
+          >{{post.title}} -- {{ post.description }}</li>
+        </ul>
+      </template>
+    </ApolloQuery>
   </v-container>
 </template>
+
+<script>
+import { gql } from "apollo-boost";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      posts: [],
+      getPostsQuery: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            categories
+            description
+            likes
+          }
+        }
+      `
+    };
+  }
+};
+</script>
