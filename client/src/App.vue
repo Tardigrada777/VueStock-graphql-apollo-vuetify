843eb10d4006899f59ff458559cee46b68229fc8
tardigrada777 <ntwrite@gmail.com>
2019-07-03 21:45:46 +0300

    Top Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,56 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+    <!-- Horizontal navbar -->
+    <v-toolbar fixed dark color="primary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-toolbar-title class="hidden-xs-only">
+        <router-link to="/" tag="span" style="cursor: pointer;">VueStock</router-link>
       </v-toolbar-title>
+
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <!-- Search input -->
+      <v-text-field flex prepend-icon="search" placeholder="Search posts" color="white"></v-text-field>
+
+      <v-spacer></v-spacer>
+      <!-- Horizontal Navbar Links -->
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="(item, i) in navItems" :to="item.link" :key="i">
+          <v-icon class="hidden-sm-only" left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
 
+    <!-- App content -->
     <v-content>
-      <HelloWorld/>
+      <v-container class="mt-4">
+        <router-view />
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {};
   },
-  data () {
-    return {
-      //
+  computed: {
+    navItems() {
+      return [
+        { icon: "chat", title: "Posts", link: "/posts" },
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "create", title: "Sign Up", link: "/signup" }
+      ];
     }
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
+</style>
