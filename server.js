2fe7665546c5b9f316c6298c425b29fc171ec8fe
tardigrada777 <ntwrite@gmail.com>
2019-07-03 10:44:17 +0300

    Инициализация клиента на vue

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 
+// import typeDefs and reslovers
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 const resolvers = require("./resolvers.js");
 
+// import env vars and mongoose models
 require("dotenv").config({ path: ".env" });
 
 const User = require("./models/User");
 const Post = require("./models/Post");
 
+// connect to mlab db
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => {
@@ -19,7 +22,7 @@ mongoose
   })
   .catch(err => console.log(err));
 
-// server
+// create Apollo/GraphQL server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -29,6 +32,7 @@ const server = new ApolloServer({
   }
 });
 
+// node server init
 server.listen().then(({ url }) => {
   console.log(`Server listening on ${url}`);
 });
